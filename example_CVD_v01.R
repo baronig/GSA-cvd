55bedf9939c3705028dd8808f12466ce19cbf0ca
GBaroni <61226928+baronig@users.noreply.github.com>
2020-02-25 14:42:43 +0100

    fixed variable name

--- example_CVD_v01.R
+++ example_CVD_v01.R
@@ -36,13 +36,13 @@ source("CVD_function.R")
 # Ishigami-Homma (3 factors)
 
 ai  = c(2, 1)          # fixed parameters
-n.k = 3                # number of factors
+n_k = 3                # number of factors
 lb  = c(-pi, -pi, -pi) # lower boundary
 ub  = c(pi, pi, pi)    # upper boundary
 
-e.sam = 14      # exponent of the number of samples e.g., = 14 
+e_sam = 14      # exponent of the number of samples e.g., = 14 
 N     = 2^e.sam # number of samples N = as power of 2 as in Sobol seq
-n.m   = 10      # number of slides m
+n_m   = 10      # number of slides m
 
 Ishigami_Homma_3 = function(x)
 {  
@@ -54,24 +54,24 @@ model_f = Ishigami_Homma_3 #choose function
 # Initialize vector and matrix
 
 vals = c() #specifications for Sobol sampling
-for (k in 1:n.k){
+for (k in 1:n_k){
   # k=1
   temp = list(list(var=paste("x", k, sep=""), dist="unif", params = list(min = lb[k], max = ub[k])))
   vals = c(vals, temp) 
 }
 
-n.seed   = 1548674 
-n.scramb = 1 # type of scrambling
+n_seed   = 1548674 
+n_scramb = 1 # type of scrambling
 
 
 # 2. Sobol sampling ==============================================
 
-X.sam = makeMCSample(N, vals, n.scramb, n.seed)
+X_sam = makeMCSample(N, vals, n_scramb, n_seed)
 
 
 # 3 run the model ==============================================
 
-Y.sam = model_f(X.sam)
+Y_sam = model_f(X_sam)
 
 # 4 visualize input-output space ==============================================
 layout(matrix(c(1:3), nr=1, byrow=F))
@@ -79,11 +79,11 @@ par(oma=c(4, 3, 1, 1))
 par(mar=c(0.5, 2.5, 1, 0))
 par(mgp=c(0.5, 0.8, 0))
 
-col.sel = c("#386cb0", "#f0027f", "#7fc97f")
+col_sel = c("#386cb0", "#f0027f", "#7fc97f")
 
-for (k in 1:n.k){
+for (k in 1:n_k){
   if(k==1) yaxt=NULL else yaxt="n"
-  plot(X.sam[, k], Y.sam, yaxt=yaxt, ylab="", xlab="", xlim=range(X.sam[, k]), ylim=range(Y.sam), col=col.sel[1], pch=1)
+  plot(X_sam[, k], Y_sam, yaxt=yaxt, ylab="", xlab="", xlim=range(X_sam[, k]), ylim=range(Y_sam), col=col_sel[1], pch=1)
     if (k==1) mtext(expression(italic("Y")), side=2, line = 2)
   mtext(bquote(italic("x")[.(k)]), side =1, line = 2.5)
   abline(h=0, lty=1, col=1)
@@ -92,11 +92,11 @@ for (k in 1:n.k){
 
 # 5: sensitivity analysis ==============================================
 
-index = CVD(X.sam, Y.sam, n.m)
+index = CVD(X_sam, Y_sam, n_m)
 
 # plot indices
 par(mfrow=c(1, 1))
-plot(index[, 1], index[, 2], type="p", xlim=c(0, 0.8), ylim=c(0, 0.8), pch=19, col=col.sel, ylab="", cex = 1.5)
+plot(index[, 1], index[, 2], type="p", xlim=c(0, 0.8), ylim=c(0, 0.8), pch=19, col=col_sel, ylab="", cex = 1.5)
 abline(v=0.1, col="gray", lty=2)
 abline(h=0.1, col="gray", lty=2)
 mtext(expression("Main effect "*italic("S"[i])), 1, line=2.5)
