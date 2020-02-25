86ed4e7d9b1a2527e5045843c6a5da786ebbf58a
GBaroni <61226928+baronig@users.noreply.github.com>
2020-02-25 14:47:50 +0100

    fixed variable names

--- CVD_function.R
+++ CVD_function.R
@@ -16,70 +16,70 @@
 # University of Potsdam (Germany)
 
 # ===================================================
-#  y.sam = output
-#  X.sam = input
+#  Y = output
+#  X = input
 #  m = number of slides
 # ===================================================
 
 CVD = function(X, Y, m) {
 
   # initialize  
-  Si      = rep(NA, n.k)                   # Si based on CVD
-  Ii      = rep(NA, n.k)                   # interaction terms 
-  MCY     = array(NA, c(n.m, n.k))         # initialize mean of the conditional Y
-  n.comb  = factorial(n.m)/(factorial(2)
-                      *factorial(n.m-2))   # number of combinations
-  Ii.temp = array(NA, c(n.comb, n.k))      # interaction indices
-  y.F     = array(NA, c(N, n.m, n.k))      # conditional samples
+  Si      = rep(NA, n_k)                   # Si based on CVD
+  Ii      = rep(NA, n_k)                   # interaction terms 
+  MCY     = array(NA, c(n_m, n_k))         # initialize mean of the conditional Y
+  n_comb  = factorial(n_m)/(factorial(2)
+                      *factorial(n_m-2))   # number of combinations
+  Ii_temp = array(NA, c(n_comb, n_k))      # interaction indices
+  y_F     = array(NA, c(N, n_m, n_k))      # conditional samples
 
   # calculations
-  VUY = var(Y.sam) # variance of the unconditional Y
+  VUY = var(Y_sam) # variance of the unconditional Y
   
   # ============================================
   # to estimate main effect
-  for (k in 1:n.k){
+  for (k in 1:n_k){
     # k=1
-    qua.x <- quantile(X[, k], probs=seq(0, 1, (1/n.m)), na.rm=TRUE)
+    qua_x = quantile(X[, k], probs = seq(0, 1, (1/n_m)), na.rm=TRUE)
     
-    for (m in 1:n.m) {
+    for (m in 1:n_m) {
   
-      y.F.temp = Y[X.sam[, k] >= qua.x[m] & X.sam[, k] < qua.x[m+1]]
-      MCY[m, k]  = mean(y.F.temp, na.rm=T)                        # calculate conditional mean
-      y.F[(1:length(y.F.temp)), m, k] = y.F.temp - MCY[m, k]       # store the centered conditional distributions
+      y_F_temp = Y[X_sam[, k] >= qua_x[m] & X_sam[, k] < qua_x[m+1]]
+      MCY[m, k]  = mean(y_F_temp, na.rm=T)                        # calculate conditional mean
+      y_F[(1:length(y_F_temp)), m, k] = y_F_temp - MCY[m, k]       # store the centered conditional distributions
       
     } #end loop over n.m
 
-    s      = smooth.spline(X[, k], Y, nknots = n.m)
+    s      = smooth.spline(X[, k], Y, nknots = n_m)
     Si[k]  = var(s$y, na.rm=T)/VUY
       
   } # loop over k
   
   # ============================================
   # Interaction index based on conditional distribution
-  for (k in 1:n.k){
+  for (k in 1:n_k){
 
     j = 1
-    for (m1 in 1:(n.m-1)) {
+    for (m1 in 1:(n_m-1)) {
       # m1=1
-      y.F1 = y.F[, m1, k]
-      y.F1 = y.F1[!is.na(y.F1)]
+      y_F1 = y_F[, m1, k]
+      y_F1 = y_F1[!is.na(y_F1)]
       
-      for (m2 in (m1+1):n.m) {
+      for (m2 in (m1+1):n_m) {
         
-        y.F2 = y.F[, m2, k]
-        y.F2 = y.F2[!is.na(y.F2)]
+        y_F2 = y_F[, m2, k]
+        y_F2 = y_F2[!is.na(y_F2)]
         
-        ub = max(y.F1, y.F2)
-        lb = min(y.F1, y.F2)
-        df.F1 = kde(x=y.F1, xmin=lb, xmax=ub)
-        df.F2 = kde(x=y.F2, xmin=lb, xmax=ub)
-        Ii.temp[j, k] = sum(abs(df.F1$estimate/sum(df.F1$estimate)-df.F2$estimate/sum(df.F2$estimate)))
+        ub = max(y_F1, y_F2)
+        lb = min(y_F1, y_F2)
+        df_F1 = kde(x = y_F1, xmin = lb, xmax = ub)
+        df_F2 = kde(x = y_F2, xmin = lb, xmax = ub)
+        Ii_temp[j, k] = sum(abs(df.F1$estimate/sum(df.F1$estimate)-df.F2$estimate/sum(df.F2$estimate)))
   
         j = j +1
       }
     }
     
-    Ii[k] = 0.5*mean(Ii.temp[, k], na.rm = T)      
+    Ii[k] = 0.5*mean(Ii_temp[, k], na.rm = T)      
  
   } # loop over k  
   
@@ -88,4 +88,3 @@ CVD = function(X, Y, m) {
 
 } # end function
 
-  
